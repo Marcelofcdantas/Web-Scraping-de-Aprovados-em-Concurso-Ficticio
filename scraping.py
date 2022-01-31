e74a6411a0b46b336fbf54b6237c7cadaea89d12
marcelofcdantas <marcelofcd@gmail.com>
2022-01-31 15:19:26 -0300

    pegando dados na pagina principal

--- scraping.py
+++ scraping.py
@@ -11,11 +11,23 @@ class Scraping:
             sleep(delay)
             response = requests.get(url, timeout=timeout)
         except (requests.ReadTimeout, requests.HTTPError):
-            return None
+            return 'Erro na resposta ou no endereço'
         else:
             if response.status_code == 200:
-                return response.text
-
-
+                aproved_data = Selector(text=response.text)
+                data = aproved_data.css("body > li > a ::text").getall()
+                id = 0
+                for cpfs in aproved_data.css('li'):
+                    page = cpfs.css("a::attr(href)").get()
+                    individual_response = requests.get(url + page)
+                    result = Selector(individual_response.text)
+                    cpf = data[id]
+                    name = result.css("body > div:nth-child(2)::text").get()
+                    score = result.css("body > div:nth-child(3)::text").get()
+                    print(cpf)
+                    print(name)
+                    print(score)
+                    id += 1
+                    
 
 print(Scraping.fetch())
